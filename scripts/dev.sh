6cd12c073c28992afdac960fbdf2c6297c72fa04
Morris Brodersen <mb@morrisbrodersen.de>
2024-08-22 20:24:23 +0200

    improve scripts and tsconfig

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -8,6 +8,6 @@ bash scripts/vendor.sh
 
 (trap 'kill 0' SIGINT; \
   tsc --project tsconfig.dev.json --watch --preserveWatchOutput & \
-  sass --watch src/css/main.scss public/css/main.css & \
+  sass --no-source-map --watch src/css/main.scss public/css/main.css & \
   s4d --spa public & \
   wait)
